c5ac0e1a61edee376ec49d3deea527f4f767fe0b
hutli <jstarm13@student.aau.dk>
2016-04-14 20:29:40 +0200

    More factory stuff

---
+++ csharp/SampleStrategy/Runner.cs
@@ -0,0 +1,75 @@
+﻿using ArtificialNeuralNetwork;
+using ArtificialNeuralNetwork.ActivationFunctions;
+using ArtificialNeuralNetwork.Factories;
+using ArtificialNeuralNetwork.WeightInitializer;
+using NeuralNetwork.GeneticAlgorithm;
+using NeuralNetwork.GeneticAlgorithm.Evaluatable;
+using NeuralNetwork.GeneticAlgorithm.Evolution;
+using NeuralNetwork.GeneticAlgorithm.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SampleStrategyStrategy
+{
+    class Runner
+    {
+        public const int HISTORYSIZE = 3;
+
+        public static object MyEvaluatableFactory { get; private set; }
+
+        public static List<double> LoadJson()
+        {
+            using (StreamReader r = new StreamReader("C:/Users/hutli/Documents/BlackBox/javascript/src/data/DANSKE.CO.json"))
+            {
+                string json = r.ReadToEnd();
+                firstItem array = JsonConvert.DeserializeObject<firstItem>(json);
+                IEnumerable<Item> arr = array.Prices;
+                return arr.Select(itm => itm.Value).Take(5).ToList();
+            }
+        }
+
+        public static void run()
+        {
+            var networkFactory = NeuralNetworkFactory.GetInstance();
+            var evalWorkingSetFactory = EvalWorkingSetFactory.GetInstance();
+            var evaluatableFactory = new EvalFact(LoadJson());
+            var randomInit = new RandomWeightInitializer(new Random());
+            var breederFactory = BreederFactory.GetInstance(networkFactory, randomInit);
+            var mutatorFactory = MutatorFactory.GetInstance(networkFactory, randomInit);
+            IGeneticAlgorithmFactory factory = GeneticAlgorithmFactory.GetInstance(networkFactory, evalWorkingSetFactory, evaluatableFactory, breederFactory, mutatorFactory);
+
+            NeuralNetworkConfigurationSettings networkConfig = new NeuralNetworkConfigurationSettings
+            {
+                NumInputNeurons = HISTORYSIZE,
+                NumOutputNeurons = 1,
+                NumHiddenLayers = 2,
+                NumHiddenNeurons = 3,
+                SummationFunction = new SimpleSummation(),
+                ActivationFunction = new TanhActivationFunction()
+            };
+            IGeneticAlgorithm evolver = factory.Create(networkConfig);
+            evolver.RunSimulation();
+        }
+
+        static void main()
+        {
+            run();
+        }
+
+    }
+
+    public class firstItem
+    {
+        public String Ticker;
+        public List<Item> Prices;
+    }
+
+    public class Item
+    {
+        public DateTime Date;
+        public double Value;
+    }
+}
---
+++ csharp/SampleStrategy/factoryShit.cs
@@ -0,0 +1,69 @@
+﻿using ArtificialNeuralNetwork;
+using NeuralNetwork.GeneticAlgorithm.Evaluatable;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SampleStrategyStrategy
+{
+    public class EvalFact : IEvaluatableFactory
+    {
+        List<double> _data;
+        public EvalFact(List<double> data)
+        {
+            _data = data;
+        }
+        public IEvaluatable Create(INeuralNetwork neuralNetwork)
+        {
+            return new Eval(_data, neuralNetwork);
+        }
+    }
+
+    public class Eval : IEvaluatable
+    {
+        private readonly INeuralNetwork _neuralNet;
+        private readonly List<double> _dataPoints;
+        public Eval(List<double> dataPoints, INeuralNetwork neuralNet)
+        {
+            _neuralNet = neuralNet;
+            _dataPoints = dataPoints;
+        }
+        public double GetEvaluation()
+        {
+            return profit;
+        }
+
+        double profit = 0;
+
+        public void RunEvaluation()
+        {
+            bool inStock = false;
+            for(int i = Runner.HISTORYSIZE; i < _dataPoints.Count() - 1; i++)
+            {
+                _neuralNet.SetInputs(_dataPoints.GetRange(i - Runner.HISTORYSIZE, Runner.HISTORYSIZE).ToArray());
+                _neuralNet.Process(); // Do magic!
+                double tomorrowsPrice = _neuralNet.GetOutputs()[0];
+                if (inStock)
+                {
+                    if(tomorrowsPrice < _dataPoints[i])
+                    {
+                        profit += _dataPoints[i];
+                        inStock = false;
+                    }
+                } else
+                {
+                    if (tomorrowsPrice > _dataPoints[i])
+                    {
+                        profit -= _dataPoints[i];
+                        inStock = true;
+                    }
+                }
+
+            }
+            if (inStock)
+            {
+                profit += _dataPoints.Last();
+            }
+
+        }
+    }
+}
